9c5cb8f77103c24d95130348d56c768b77723b29
CynthiaKamau <cynthiakamau54@gmail.com>
2024-07-15 13:31:47 +0300

    Add care and treatment clinical views submenu

--- src/dashboard.meta.ts
+++ src/dashboard.meta.ts
@@ -4,3 +4,25 @@ export const dashboardMeta = {
   path: "Encounters",
   title: "Encounters",
 };
+
+export const hivPatientChartMeta = {
+  title: "HIV Care and Treatment",
+  slotName: "hiv-patient-chart-slot",
+  isExpanded: false,
+};
+
+export const hivPatientSummaryDashboardMeta = {
+  slot: "hiv-patient-summary-slot",
+  columns: 1,
+  title: "Patient Summary",
+  path: "hiv-patient-summary",
+  layoutMode: "anchored",
+};
+
+export const hivPatientProgramDashboardMeta = {
+  slot: "hiv-patient-summary-slot",
+  columns: 1,
+  title: "Program Management",
+  path: "hiv-program-management",
+  layoutMode: "anchored",
+};
---
+++ src/patient-summary/patient-summary.component.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PatientSummary = () => {
+  return <div>Patient Summary</div>;
+};
+
+export default PatientSummary;
