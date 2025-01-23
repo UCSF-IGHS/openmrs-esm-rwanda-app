3a0e99906c3a2a2c8285d241dc6725c1faae8681
CynthiaKamau <cynthiakamau54@gmail.com>
2025-01-23 15:32:49 +0300

    removed unused dependancies

--- src/patient-summary/patient-summary.component.tsx
+++ src/patient-summary/patient-summary.component.tsx
@@ -1,20 +1,10 @@
-import { useConfig } from "@openmrs/esm-framework";
 import React from "react";
 import styles from "../common.scss";
 import patientSummaryConfigWorkflow from "./patient-summary-config.json";
-import {
-  getSummaryCardProps,
-  SummaryCard,
-} from "@ohri/openmrs-esm-ohri-commons-lib";
 import { Tile } from "@carbon/react";
 import { CardHeader } from "@openmrs/esm-patient-common-lib";
 
-interface PatientSummaryProps {
-  patientUuid: string;
-}
-
-const PatientSummary: React.FC<PatientSummaryProps> = ({ patientUuid }) => {
-  const config = useConfig();
+const PatientSummary = () => {
   const cards = patientSummaryConfigWorkflow.cardDefinitions;
 
   return (
