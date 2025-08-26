7f781f4094ec86952563f476d531e9324d85144b
CynthiaKamau <cynthiakamau54@gmail.com>
2025-08-26 14:06:52 +0300

    Update encounters link (#25)

--- src/dashboard.meta.ts
+++ src/dashboard.meta.ts
@@ -1,7 +1,6 @@
 export const dashboardMeta = {
   slot: "patient-chart-all-encounters-dashboard-slot",
-  columns: 1,
   path: "Encounters",
-  title: "Encounters",
+  title: "Visits",
   icon: "omrs-icon-group",
 };
